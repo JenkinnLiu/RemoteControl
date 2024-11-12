da28cc12730700e201ddd34741e77d4fcb4a9b5f
JenkinnLiu <1419359341@qq.com>
2024-11-12 21:56:37 +0800

    完成鼠标消息的处理

--- RemoteControl/RemoteControl.cpp
+++ RemoteControl/RemoteControl.cpp
@@ -140,6 +140,100 @@ int DownloadFile() {
     return 0;
 }
 
+int MouseEvent() {
+    MOUSEEV mouse;
+    if (CServerSocket::getInstance()->GetMouseEvent(mouse)) {
+        DWORD nflags = 0;
+        switch (mouse.nButton){
+            case 0://左键
+                nflags = 1;
+                break;
+            case 1://右键
+                nflags = 2;
+                break;
+            case 2://中键
+                nflags = 4;
+                break;
+            case 4://没有按键
+                nflags = 8;
+
+        default:
+            break;
+        }
+        if (nflags != 8)SetCursorPos(mouse.ptXY.x, mouse.ptXY.y);
+        switch (mouse.nAction){
+            case 0://单击
+                nflags |= 0x10;
+                break;
+            case 1://双击
+                nflags |= 0x20;
+                break;
+            case 2://按下
+                nflags |= 0x40;
+                break;
+            case 3://放开
+                nflags |= 0x80;
+                break;
+        default:
+            break;
+        }
+        switch (nflags){
+			//mouse_event()用于模拟鼠标操作
+            case 0x21://左键双击，先执行双击的两个mouse_event,再执行单击的两个mouse_event
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, GetMessageExtraInfo());
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, GetMessageExtraInfo());
+            case 0x11://左键单击
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, GetMessageExtraInfo());
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, GetMessageExtraInfo());
+                break;
+            case 0x41://左键按下
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, GetMessageExtraInfo());
+                break;
+            case 0x81://左键放开
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, GetMessageExtraInfo());
+                break;
+            case 0x22://右键双击
+                mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, GetMessageExtraInfo());
+                mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, GetMessageExtraInfo());
+            case 0x12://右键单击
+                mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, GetMessageExtraInfo());
+                mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, GetMessageExtraInfo());
+                break;
+            case 0x42://右键按下
+                mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, GetMessageExtraInfo());
+                break;
+            case 0x82://右键放开
+                mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, GetMessageExtraInfo());
+                break;
+            case 0x24://中键双击
+                mouse_event(MOUSEEVENTF_MIDDLEDOWN, 0, 0, 0, GetMessageExtraInfo());
+                mouse_event(MOUSEEVENTF_MIDDLEUP, 0, 0, 0, GetMessageExtraInfo());
+            case 0x14://中键单击
+                mouse_event(MOUSEEVENTF_MIDDLEDOWN, 0, 0, 0, GetMessageExtraInfo());
+                mouse_event(MOUSEEVENTF_MIDDLEUP, 0, 0, 0, GetMessageExtraInfo());
+                break;
+            case 0x44://中键按下
+                mouse_event(MOUSEEVENTF_MIDDLEDOWN, 0, 0, 0, GetMessageExtraInfo());
+                break;
+            case 0x84://中键放开
+                mouse_event(MOUSEEVENTF_MIDDLEUP, 0, 0, 0, GetMessageExtraInfo());
+                break;
+            case 0x08://单纯鼠标移动
+                mouse_event(MOUSEEVENTF_MOVE, mouse.ptXY.x, mouse.ptXY.y, 0, GetMessageExtraInfo());
+                break;
+        default:
+            break;
+        }
+        CPacket pack(4, NULL, 0);//操作好了就传一个NULL，表明消息执行完成
+        CServerSocket::getInstance()->Send(pack);
+    }
+    else {
+        OutputDebugString(_T("获取鼠标操作参数失败！！"));
+        return -1;
+    }
+    return 0;
+}
+
 int main()
 {
     int nRetCode = 0;
@@ -191,6 +285,9 @@ int main()
                 break;
             case 4://下载文件
                 DownloadFile();
+                break;
+            case 5://鼠标操作
+                MouseEvent();
                 break;
             }
 		}
