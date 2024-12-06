24bd14c6ebcaceaebae336828a4577769c7956c2
JenkinnLiu <1419359341@qq.com>
2024-12-06 22:36:19 +0800

    1. 代码重构 2. 代码优化

--- RemoteControl/RemoteControl.cpp
+++ RemoteControl/RemoteControl.cpp
@@ -8,11 +8,14 @@
 #include "Tool.h"
 #include "Command.h"
 
+
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
 // 唯一的应用程序对象
+#define INVOKE_PATH _T("C:\\Users\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RemoteControl.exe")
+#define INVOKE_PATH2 _T("C:\\Windows\\SysWOW64\\RemoteControl.exe")
 
 CWinApp theApp;
 using namespace std;
@@ -20,22 +23,23 @@ using namespace std;
 void WriteRigisterTable() {
     CString strSubKey = _T("\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
     CString strPath = CString(_T("C:\\Windows\\SysWOW64\\RemoteControl.exe"));
-    char sPath[MAX_PATH] = "";
-    char sSys[MAX_PATH] = "";
-    std::string strExe = "\\RemoteControl.exe ";
-    GetCurrentDirectoryA(MAX_PATH, sPath);
-    GetSystemDirectoryA(sSys, sizeof sSys);
-    std::string strCmd = "mklink " + std::string(sSys) + std::string(sPath) + "\\RemoteControl.exe";
-    system(strCmd.c_str());//执行命令
+    TCHAR sPath[MAX_PATH] = _T("");
+    GetModuleFileName(NULL, sPath, MAX_PATH);
+    BOOL ret = CopyFile(sPath, strPath, FALSE);
+    //fopen CFile system(copy) CopyFile OpenFile
+    if (ret == FALSE) {
+        MessageBox(NULL, _T("复制文件失败！是否权限不足？"), _T("错误"), MB_OK | MB_ICONERROR);
+        ::exit(0);
+    }
     HKEY hKey = NULL;
-    int ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &hKey);//打开注册表
+    ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &hKey);//打开注册表
     if (ret != ERROR_SUCCESS) {
         RegCloseKey(hKey);
         MessageBox(NULL, _T("打开注册表失败！是否权限不足？"), _T("开机启动错误"), MB_OK | MB_ICONERROR);
         ::exit(0);
     }
 
-    int ret = RegSetValueEx(hKey, _T("RemoteControl"), 0, REG_SZ, (BYTE*)(LPCTSTR)strPath, strPath.GetLength() * sizeof(TCHAR));//写入注册表
+    ret = RegSetValueEx(hKey, _T("RemoteControl"), 0, REG_SZ, (BYTE*)(LPCTSTR)strPath, strPath.GetLength() * sizeof(TCHAR));//写入注册表
     if (ret != ERROR_SUCCESS) {
         RegCloseKey(hKey);
         MessageBox(NULL, _T("写入注册表失败！是否权限不足？"), _T("开机启动错误"), MB_OK | MB_ICONERROR);
@@ -46,10 +50,11 @@ void WriteRigisterTable() {
 
 //开机启动的第二种方法，复制到Windows启动目录
 void WriteStartupDir(CString strPath) {
+	
+    TCHAR sPath[MAX_PATH] = _T("");
+    GetModuleFileName(NULL, sPath, MAX_PATH);
     //CString strPath = _T("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\RemoteControl.exe");
-    CString strCmd = GetCommandLine();
-	strCmd.Replace(_T("\""), _T(""));//把双引号去掉
-    BOOL ret = CopyFile(strCmd, strPath, FALSE);
+    BOOL ret = CopyFile(sPath, strPath, FALSE);
     //fopen CFile system(copy) CopyFile OpenFile
     if (ret == FALSE) {
 		MessageBox(NULL, _T("复制文件失败！是否权限不足？"), _T("错误"), MB_OK | MB_ICONERROR);
@@ -63,11 +68,11 @@ void WriteStartupDir(CString strPath) {
 //1.复制这些dll到system32下或者sysWOW64下，或者将程序放到系统目录下
 //system32下面多是64位的dll，sysWOW64下面多是32位的dll
 //2.使用静态库，而非动态库
-void ChooseAutoInvoke() {//自动启动
+bool ChooseAutoInvoke(const CString& strPath) {//自动启动
 	TCHAR wcsSystem[MAX_PATH] = _T("");
-    CString strPath = CString(_T("C:\\Windows\\SysWOW64\\RemoteControl.exe"));
+    //CString strPath = CString(_T("C:\\Windows\\SysWOW64\\RemoteControl.exe"));
     if(PathFileExists(strPath)){
-        return;
+        return false;
     }
     
 	CString strInfo = _T("该程序只能用于合法的用途！");
@@ -77,12 +82,13 @@ void ChooseAutoInvoke() {//自动启动
 	strInfo += _T("请问是否继续？");
 	int ret = MessageBox(NULL, strInfo, _T("警告"), MB_YESNOCANCEL | MB_ICONWARNING | MB_TOPMOST);
 	if (ret == IDCANCEL) {
-		::exit(0);
+        return false;
     }
     else if (ret == IDOK) {
 		//WriteRigisterTable(strPath);//第一种方法
 		WriteStartupDir(strPath);//第二种方法
     }
+    return true;
     //GPT版自动启动
 	/*HKEY hKey;*/
 	//if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"), 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS) {//打开注册表
@@ -99,91 +105,51 @@ void ChooseAutoInvoke() {//自动启动
 	//else {
 	//	MessageBox(NULL, _T("自动启动失败！"), _T("自动启动"), MB_OK | MB_ICONERROR);
 	//}
-
-
 }
 
-void ShowError() {
-    LPVOID lpMessageBuf = NULL;
-    //strerror(errno);//标准C语言库
-	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
-        NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), 
-        (LPWSTR)&lpMessageBuf, 0, NULL);//windows API
-    OutputDebugString(lpMessageBuf);
-    LocalFree(lpMessageBuf);
-}
 
-bool IsAdmin() {//检测是否为管理员
-    HANDLE hToken = NULL;
-	if (!OpenProcessToken(GetCurrentProcess, TOKEN_QUERY, &hToken)) {//打开进程令牌,获取当前进程的访问令牌
-        ShowError();
+bool Init() {
+    HMODULE hModule = ::GetModuleHandle(nullptr);
+	if (hModule == nullptr) {
+        wprintf(L"错误: MFC 初始化失败\n");
+		return false;
+	}
+    if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+    {
+        // TODO: 在此处为应用程序的行为编写代码。
+        wprintf(L"错误: MFC 初始化失败\n");
         return false;
     }
-	TOKEN_ELEVATION eve;//提权令牌
-    DWORD len = 0;
-    if(GetTokenInformation(hToken, TokenElevation, &eve, sizeof(eve), &len) == false){//获取令牌信息
-        ShowError();
-        return false;
-    }
-    if (len == sizeof eve) {
-		CloseHandle(hToken);
-		return eve.TokenIsElevated;//当前令牌是否可以提权
-    }
-	TRACE("length of tokeninformation is %d\r\n", len);
-    return false;
+    return true;
 }
 
 int main()
 {
-    if (IsAdmin()) {
+    if (CTool::IsAdmin()) {
+		if (!Init()) return 1;//初始化失败
         OutputDebugString(L"current is run as administartor!\r\n");
+        //MessageBox(NULL, _T("管理员"), _T("用户状态"), 0);
+        CCommand cmd;
+		if (!ChooseAutoInvoke(INVOKE_PATH)) ::exit(0);//自动启动
+        //转换成void*类型是最好加一个强制类型转换reinterpret_cast<SOCKET_CALLBACK>
+        int ret = CServerSocket::getInstance()->Run(reinterpret_cast<SOCKET_CALLBACK>(&CCommand::RunCommand), &cmd);//run函数中调用了InitSocket
+        switch (ret) {
+        case -1:
+            MessageBox(NULL, _T("网络初始化异常，未能成功初始化，请检查网络状态！"), _T("网络初始化失败！"), MB_OK | MB_ICONERROR);
+            break;
+        case -2:
+            MessageBox(NULL, _T("多次无法正常接入用户，结束程序！"), _T("客户端连接失败！"), MB_OK | MB_ICONERROR);
+            break;
+        }
     }
     else {
         OutputDebugString(L"current is not run as normal user!\r\n");
-    }
-    int nRetCode = 0;
-
-    HMODULE hModule = ::GetModuleHandle(nullptr);
-
-    if (hModule != nullptr)
-    {
-        // 初始化 MFC 并在失败时显示错误。。
-        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
-        {
-            // TODO: 在此处为应用程序的行为编写代码。
-            wprintf(L"错误: MFC 初始化失败\n");
-            nRetCode = 1;
+        if (CTool::RunAsAdmin() == false) {
+            CTool::ShowError();
+            return 1;
         }
-        else
-        {
-			CCommand cmd;
-            //转换成void*类型是最好加一个强制类型转换reinterpret_cast<SOCKET_CALLBACK>
-            int ret = CServerSocket::getInstance()->Run(reinterpret_cast<SOCKET_CALLBACK>(&CCommand::RunCommand), &cmd);//run函数中调用了InitSocket
-            switch (ret) {
-            case -1:
-                MessageBox(NULL, _T("网络初始化异常，未能成功初始化，请检查网络状态！"), _T("网络初始化失败！"), MB_OK | MB_ICONERROR);
-				exit(0);
-				break;
-            case -2:
-                MessageBox(NULL, _T("多次无法正常接入用户，结束程序！"), _T("客户端连接失败！"), MB_OK | MB_ICONERROR);
-                exit(0);
-                break;
-            }
-			
-          
-		}
-        // TODO: 在此处为应用程序的行为编写代码。
-        //WSADATA data;
-        //WSAStartup(MAKEWORD(1, 1), &data);//TODO: 返回值处理
-
-            
+        //获取管理员权限，使用该权限创建进程
+        //MessageBox(NULL, _T("普通用户"), _T("用户状态"), 0);
+        return 0;
     }
-    else
-    {
-        // TODO: 更改错误代码以符合需要
-        wprintf(L"错误: GetModuleHandle 失败\n");
-        nRetCode = 1;
-    }
-
-    return nRetCode;
 }
--- RemoteControl/Tool.h
+++ RemoteControl/Tool.h
@@ -13,5 +13,54 @@ public:
         strOut += "\n";
         OutputDebugStringA(strOut.c_str());
     }
+
+    static bool IsAdmin() {//检测是否为管理员
+        HANDLE hToken = NULL;
+        if (!OpenProcessToken(GetCurrentProcess, TOKEN_QUERY, &hToken)) {//打开进程令牌,获取当前进程的访问令牌
+            ShowError();
+            return false;
+        }
+        TOKEN_ELEVATION eve;//提权令牌
+        DWORD len = 0;
+        if (GetTokenInformation(hToken, TokenElevation, &eve, sizeof(eve), &len) == false) {//获取令牌信息
+            ShowError();
+            return false;
+        }
+        if (len == sizeof eve) {
+            CloseHandle(hToken);
+            return eve.TokenIsElevated;//当前令牌是否可以提权
+        }
+        TRACE("length of tokeninformation is %d\r\n", len);
+        return false;
+    }
+    static bool RunAsAdmin() {//提权,以管理员的方式启动
+        //本地策略组， 开启Administrator账户， 禁止空密码只能登录本地控制台
+        STARTUPINFO si = { 0 };//进程启动信息
+        _PROCESS_INFORMATION pi = { 0 };//进程信息
+        TCHAR sPath[MAX_PATH] = _T("");
+        GetModuleFileName(NULL, sPath, MAX_PATH);//获取当前模块项目的文件名
+
+        //ret = CreateProcessWithTokenW(hToken, LOGON_WITH_PROFILE, NULL, (LPWSTR)(LPCWSTR)strCmd, CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);//创建进程
+        BOOL ret = CreateProcessWithLogonW(_T("Administrator"), NULL, NULL, LOGON32_LOGON_BATCH, NULL, sPath, CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);//创建进程,以管理员的方式启动
+        if (!ret) {
+            //ShowError();
+            MessageBox(NULL, _T("创建进程失败"), _T("错误"), 0);
+            return false;
+        }
+        WaitForSingleObject(pi.hProcess, INFINITE);//等待进程结束
+        CloseHandle(pi.hProcess);//关闭进程句柄
+        CloseHandle(pi.hThread);//关闭线程句柄
+		return true;
+    }
+    static void ShowError() {
+        LPWSTR lpMessageBuf = NULL;
+        //strerror(errno);//标准C语言库
+        FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+            NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            (LPWSTR)&lpMessageBuf, 0, NULL);//windows API
+        OutputDebugString(lpMessageBuf);
+        LocalFree(lpMessageBuf);
+    }
+
 };
 
